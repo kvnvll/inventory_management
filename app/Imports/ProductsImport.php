dcf43ca1428f0a3cfaee9772af369c44ab60fcc3
kvinV <vilakvn@gmail.com>
2026-06-07 02:01:23 +0800

    Add import dropdown for products categories suppliers

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -3,6 +3,9 @@
 namespace App\Imports;
 
 use App\Models\Product;
+use App\Models\Category;
+use App\Models\Supplier;
+
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -10,14 +13,22 @@ class ProductsImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
+        $supplier = Supplier::firstOrCreate([
+            'name' => $row['supplier']
+        ]);
+
+        $category = Category::firstOrCreate([
+            'name' => $row['category']
+        ]);
+
         return new Product([
-            'supplier_id'     => $row['supplier_id'],
-            'category_id'     => $row['category_id'],
+            'supplier_id'     => $supplier->id,
+            'category_id'     => $category->id,
             'name'            => $row['name'],
             'barcode'         => $row['barcode'],
             'price'           => $row['price'],
             'stock'           => $row['stock'],
-            'low_stock_limit' => $row['low_stock_limit'],
+            'low_stock_limit' => $row['low_stock_limit'] ?? 10,
             'expiration_date' => $row['expiration_date'],
         ]);
     }
